2bcbb3298b8544ae1fe0c9d00d518d535e9d73a0
N Hayakawa <nh@MacBook-Pro.local>
2022-01-22 22:19:07 +0900

    portfolio

--- 本原自動車教習所/js/toggle.js
+++
@@ -1,7 +0,0 @@
-$(document).on('ready', function() {
-    $(".regular").slick({
-      autoplay: true, // 自動再生を設定
-      autoplaySpeed: 4000, // 自動再生のスピード（ミリ秒単位）
-      dots: true // ドットインジケーターの表示
-    });
-  });
